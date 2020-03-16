9772d9670370f14d138dce5bfca9f60a9dba8dfb
Rustyn Simmons <Rustyn@rustynsimmons.com>
2020-03-16 16:11:49 -0700

    Update ingredient controller to find by name and id.

--- Controllers/ingredientController.js
+++ Controllers/ingredientController.js
@@ -1,4 +1,5 @@
 const Ingredient = require('../Models/ingredient');
+const Recipe = require('../Models/recipe');
 
 exports.addIngredient = (req, res, next) => {
     const ingredient = new Ingredient({
@@ -15,6 +16,68 @@ exports.addIngredient = (req, res, next) => {
         .then(result => {
             console.log('Ingredient Created');
             res.json(result);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
+
+exports.editIngredient = (req, res, next) => {
+    Ingredient.findById(req.body._id)
+        .then(ingredient => {
+                ingredient.calories = req.body.calories;
+                ingredient.servingSize = req.body.servingSize;
+                ingredient.measurement = req.body.measurement;
+                ingredient.protein = req.body.protein;
+                ingredient.fat = req.body.fat;
+                ingredient.carbohydrate = req.body.carbohydrate;
+                return ingredient.save();
+        })
+        .then(updatedIngredient => {
+            console.log('Ingredient Updated!');
+            res.json(updatedIngredient);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
+
+exports.deleteIngredient = (req, res, next) => {
+    Ingredient.findById(req.body._id)
+        .then(ingredient => {
+            ingredient.deleteOne();
+            res.send('Ingredient Deleted');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
+
+exports.getIngredientByName = (req, res, next) => {
+    Ingredient.find({name: req.body.name})
+        .then(ingredients => {
+            res.json(ingredients);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
+
+exports.getIngredient = (req, res, next) => {
+    Ingredient.findById(req.body._id)
+        .then(ingredient => {
+            res.json(ingredient);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
+
+exports.getIngredientByRecipe = (req, res, next) => {
+    Recipe.findById(req.body._id)
+        .populate()
+        .then(recipe => {
+            res.json(recipe.ingredients);
         })
         .catch(err => {
             console.log(err);
--- Routes/ingredientRoutes.js
+++ Routes/ingredientRoutes.js
@@ -6,4 +6,14 @@ const router = express.Router();
 
 router.post('/add', ingredientController.addIngredient);
 
+router.put('/edit', ingredientController.editIngredient);
+
+router.delete('/delete', ingredientController.deleteIngredient);
+
+router.get('/get-by-name', ingredientController.getIngredientByName);
+
+router.get('/get', ingredientController.getIngredient);
+
+router.get('/get-by-recipe', ingredientController.getIngredientByRecipe);
+
 module.exports = router;
